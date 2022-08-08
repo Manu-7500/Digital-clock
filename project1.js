ae64557712356748adc2bcd99c252548f7cfeee7
manu <pandatmanu318@gmail.com>
2022-08-08 12:13:40 +0300

    Digital clock

---
+++ project1.js
@@ -0,0 +1,15 @@
+const hourEl = document.getElementById( "hours" );
+const minutesEl = document.getElementById( "minutes" );
+const secondsEl = document.getElementById( "seconds" );
+const ampmEl = document.getElementById( "ampm" );
+
+function updateclock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if ( h > 12 ) {
+        h = h - 12
+        ampm = "PM"
+    }
